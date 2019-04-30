c5761028b097601c77647eff403aa92be535d1e1
Guy Taylor <thebigguy.co.uk@gmail.com>
2019-04-30 23:22:57 +0100

    Type bioarch/occupational_markers.py

--- bioarch/occupational_markers.py
+++ bioarch/occupational_markers.py
@@ -2,13 +2,13 @@
 
 
 import logging
-from typing import Any, Union
+from typing import Any, List, Union
 
 
 import pandas as pd
 
 
-from .left_right import LeftRight
+from .left_right import LeftRight, Optional
 
 
 logger = logging.getLogger(__name__)
@@ -33,7 +33,7 @@ class EnthesialMarker(object):
             raise ValueError(f'Invalid EnthesialMarker value, not a 0.5 increment: {self.value}')
 
     @staticmethod
-    def parse(value: Any):
+    def parse(value: Any) -> Optional['EnthesialMarker']:
         if value is None:
             return None
 
@@ -88,7 +88,7 @@ class EnthesialMarker(object):
         return val
 
     @staticmethod
-    def avg(left, right):
+    def avg(left: Optional['EnthesialMarker'], right: Optional['EnthesialMarker']) -> Optional['EnthesialMarker']:
         return left if right is None else right
 
     def __eq__(self, other: Any):
@@ -109,7 +109,7 @@ class EnthesialMarker(object):
 
 class OccupationalMarkers(object):  # pylint: disable=R0902
     """docstring for OccupationalMarkers"""
-    def __init__(self, c_trapezius, c_o_deltiod, c_o_pectoralis_major, c_costoclaviclar_lig, c_subcalvius, c_conoid_lig, c_trapezoid_lig, s_pectoralis_minor, s_serratus_anterior, s_triceps_long_head, s_trapezius, h_subscapularis, h_teres_major, h_latissimus_dorsi, h_pectoralis_major, h_deltoid, h_coracobrachialis, h_supraspinatus, h_infraspinatus, h_teres_minor, h_o_extensor, h_o_flexor, u_brachialis, u_o_pronator_quadrataus, u_triceps_brachii, u_anconeus, u_o_supinator, r_biceps_brachii, r_supinator, r_pronator_teres, r_pronator_quadratus, r_brachoradialis, f_gluteus_minimus, f_gluteus_medius, f_piriformus, f_obturator_internus, f_obturator_externus, f_quadratis_femoris, f_ilioposas, f_gluteus_maximus, f_pectineus, f_o_vastus_medialis, f_o_vastus_lateralis, o_adductor_magnus, f_o_gastrocnemius, f_o_plantaris, f_o_popliteus, t_tensor_fascia_latae, t_quadriceps, t_sartorius, t_gracilis, t_semitendinosus, t_o_tibialus_anterior, t_biceps_femoris, t_semimembranosus, t_popliteus, t_o_soleus, t_o_tibialis_posterior, t_o_flexor_digitorium, f_biceps_femoris, f_o_extensor_muscles, f_o_flexor_muscles, f_o_peroneus_longus, f_o_peronus_brevis, f_o_soleus, p_quadriceps, c_achilles):
+    def __init__(self, c_trapezius: LeftRight[EnthesialMarker], c_o_deltiod: LeftRight[EnthesialMarker], c_o_pectoralis_major: LeftRight[EnthesialMarker], c_costoclaviclar_lig: LeftRight[EnthesialMarker], c_subcalvius: LeftRight[EnthesialMarker], c_conoid_lig: LeftRight[EnthesialMarker], c_trapezoid_lig: LeftRight[EnthesialMarker], s_pectoralis_minor: LeftRight[EnthesialMarker], s_serratus_anterior: LeftRight[EnthesialMarker], s_triceps_long_head: LeftRight[EnthesialMarker], s_trapezius: LeftRight[EnthesialMarker], h_subscapularis: LeftRight[EnthesialMarker], h_teres_major: LeftRight[EnthesialMarker], h_latissimus_dorsi: LeftRight[EnthesialMarker], h_pectoralis_major: LeftRight[EnthesialMarker], h_deltoid: LeftRight[EnthesialMarker], h_coracobrachialis: LeftRight[EnthesialMarker], h_supraspinatus: LeftRight[EnthesialMarker], h_infraspinatus: LeftRight[EnthesialMarker], h_teres_minor: LeftRight[EnthesialMarker], h_o_extensor: LeftRight[EnthesialMarker], h_o_flexor: LeftRight[EnthesialMarker], u_brachialis: LeftRight[EnthesialMarker], u_o_pronator_quadrataus: LeftRight[EnthesialMarker], u_triceps_brachii: LeftRight[EnthesialMarker], u_anconeus: LeftRight[EnthesialMarker], u_o_supinator: LeftRight[EnthesialMarker], r_biceps_brachii: LeftRight[EnthesialMarker], r_supinator: LeftRight[EnthesialMarker], r_pronator_teres: LeftRight[EnthesialMarker], r_pronator_quadratus: LeftRight[EnthesialMarker], r_brachoradialis: LeftRight[EnthesialMarker], f_gluteus_minimus: LeftRight[EnthesialMarker], f_gluteus_medius: LeftRight[EnthesialMarker], f_piriformus: LeftRight[EnthesialMarker], f_obturator_internus: LeftRight[EnthesialMarker], f_obturator_externus: LeftRight[EnthesialMarker], f_quadratis_femoris: LeftRight[EnthesialMarker], f_ilioposas: LeftRight[EnthesialMarker], f_gluteus_maximus: LeftRight[EnthesialMarker], f_pectineus: LeftRight[EnthesialMarker], f_o_vastus_medialis: LeftRight[EnthesialMarker], f_o_vastus_lateralis: LeftRight[EnthesialMarker], o_adductor_magnus: LeftRight[EnthesialMarker], f_o_gastrocnemius: LeftRight[EnthesialMarker], f_o_plantaris: LeftRight[EnthesialMarker], f_o_popliteus: LeftRight[EnthesialMarker], t_tensor_fascia_latae: LeftRight[EnthesialMarker], t_quadriceps: LeftRight[EnthesialMarker], t_sartorius: LeftRight[EnthesialMarker], t_gracilis: LeftRight[EnthesialMarker], t_semitendinosus: LeftRight[EnthesialMarker], t_o_tibialus_anterior: LeftRight[EnthesialMarker], t_biceps_femoris: LeftRight[EnthesialMarker], t_semimembranosus: LeftRight[EnthesialMarker], t_popliteus: LeftRight[EnthesialMarker], t_o_soleus: LeftRight[EnthesialMarker], t_o_tibialis_posterior: LeftRight[EnthesialMarker], t_o_flexor_digitorium: LeftRight[EnthesialMarker], f_biceps_femoris: LeftRight[EnthesialMarker], f_o_extensor_muscles: LeftRight[EnthesialMarker], f_o_flexor_muscles: LeftRight[EnthesialMarker], f_o_peroneus_longus: LeftRight[EnthesialMarker], f_o_peronus_brevis: LeftRight[EnthesialMarker], f_o_soleus: LeftRight[EnthesialMarker], p_quadriceps: LeftRight[EnthesialMarker], c_achilles: LeftRight[EnthesialMarker]):
         self.c_trapezius = c_trapezius
         self.c_o_deltiod = c_o_deltiod
         self.c_o_pectoralis_major = c_o_pectoralis_major
@@ -179,11 +179,11 @@ class OccupationalMarkers(object):  # pylint: disable=R0902
         self.c_achilles = c_achilles
 
     @staticmethod
-    def empty():
-        markers = [LeftRight(None, None)] * 67
+    def empty() -> 'OccupationalMarkers':
+        markers: List[LeftRight[EnthesialMarker]] = [LeftRight(None, None)] * 67
         return OccupationalMarkers(*markers)
 
-    def to_pd_series(self, prefix=''):
+    def to_pd_series(self, prefix: str = '') -> pd.Series:
         labels = []
         values = []
         for key, value in self.__dict__.items():
