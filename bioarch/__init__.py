c55ebc7832016f8de5d2a474a7b0e4ae5be668a9
Guy Taylor <thebigguy.co.uk@gmail.com>
2019-04-30 23:25:17 +0100

    Bump version to v0.0.2

--- bioarch/__init__.py
+++ bioarch/__init__.py
@@ -18,7 +18,7 @@ if sys.version_info < (3, 7):
 
 
 __title__ = 'bioarch'
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__ = 'Guy Taylor'
 
 __all__ = ['AgeCategory', 'EstimatedAge'] + ['AgeSexStature', 'BurialInfo', 'Individual', 'LongBoneMeasurement', 'OsteologicalSex'] + \
