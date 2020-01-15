e01254ff65bec989b3c9733d09371c306d2fbee1
Guy Taylor <thebigguy.co.uk@gmail.com>
2020-01-15 18:10:56 +0000

    Parse and validate context as well as grouping them
    The grouping code needs improvement but this is to be done with the
    fixed grouping maps only.

--- bioarch/context.py
+++ bioarch/context.py
@@ -7,21 +7,63 @@ from typing import Dict, Optional, Union
 import pandas as pd
 
 
+def parse_value(value):
+    if value is None:
+        return None
+    if isinstance(value, str) and value.upper() == 'NA':
+        return None
+    if value in (1, '1'):
+        return True
+    if value in (0, '0'):
+        return False
+    raise ValueError(f'Failed to parse Boolean value "{value}" for context')
+
+
+KNOWN_GROUPS = {
+    'utilitarian'  : set(['knife', 'whetstone', 'awl', 'scissors', 'vessel', 'sherd', 'flint', 'flakes']),  # noqa: E203
+    'textile'      : set(['textile', 'needle', 'spindle whorl']),  # noqa: E203
+    'equestrian'   : set(['equestrian']),  # noqa: E203
+    'economic'     : set(['coins', 'scales']),  # noqa: E203
+    'organic_material': set(['animal bone', 'charcol', 'flower', 'shell', 'burned bone']),
+    'appearance'   : set(['brooch', 'cloak ring', 'bracelet', 'beads', 'comb', 'buckle', 'mount']),  # noqa: E203
+    'burial_container': set(['coffin', 'coffin substitute']),
+    'weapons'      : set(['sword', 'axe', 'iron pole', 'shield', 'spear']),  # noqa: E203
+    'iron_fragment': set(['iron frafments', 'nails']),
+    'miscellaneous': set(['lock', 'keys', 'thors hammer', 'bronze item', 'bronze disc', 'quartz']),
+}
+
+
 class Context(object):
     """docstring for Context"""
 
     def __init__(self, tags: Dict[str, Optional[Union[str, int, bool, float]]]):
-        self.tags = tags
+        self.tags = {k: parse_value(v) for k, v in tags.items()}
 
     @staticmethod
     def empty():
         return Context({})
 
-    def to_pd_data_frame(self, index, prefix=''):
-        labels = [f'{prefix}{label}' for label in self.tags.keys()]
-        s = pd.Series([val for val in self.tags.values()], index=labels, copy=True)  # pylint: disable=R1721
+    @staticmethod
+    def group(value):
+        value = value.lower()
+        groups = set()
+        for group_name, group in KNOWN_GROUPS.items():
+            if value in group:
+                groups.add(group_name)
+        return groups
 
-        return pd.DataFrame.from_dict({index: s}, orient='index')
+    def _to_pd_series(self, prefix, tags):  # pylint: disable=R0201
+        labels = [f'{prefix}{label}' for label in tags.keys()]
+        s = pd.Series([val for val in tags.values()], index=labels, copy=True)  # pylint: disable=R1721
+        s = s.append(pd.Series([s.count()], index=[f'{prefix}count']))
+        return s
+
+    def to_pd_data_frame(self, index, prefix=''):
+        group_series = [self._to_pd_series(f'{prefix}all_', self.tags)]
+        for group_name, group in KNOWN_GROUPS.items():
+            group_series.append(self._to_pd_series(f'{prefix}{group_name}_', {k: v for k, v in self.tags.items() if k.lower() in group}))
+
+        return pd.DataFrame.from_dict({index: pd.concat(group_series)}, orient='index')
 
 
 if __name__ == "__main__":
--- bioarch/context_test.py
+++ bioarch/context_test.py
@@ -8,12 +8,81 @@ from .context import Context
 
 
 class ContextTest(unittest.TestCase):
+    def test_constructor(self):
+        self.assertEqual(Context({'thing': True}).tags['thing'], True)
+        self.assertEqual(Context({'thing': False}).tags['thing'], False)
+
+        self.assertEqual(Context({'thing': None}).tags['thing'], None)
+        self.assertEqual(Context({'thing': 'NA'}).tags['thing'], None)
+        self.assertEqual(Context({'thing': 'na'}).tags['thing'], None)
+
+        with self.assertRaises(ValueError):
+            Context({'spear': 'foo'})
+
+        with self.assertRaises(ValueError):
+            Context({'spear': 10})
+
     def test_to_pd_data_frame(self):
-        context = Context({'spear': True, 'pots': 2})
+        context = Context({'spear': True,
+                           'pots': 'NA',
+                           'knife': None})
 
         df = context.to_pd_data_frame('id1', prefix='context_')
 
-        self.assertEqual(df.to_json(orient='records'), '[{"context_spear":true,"context_pots":2}]')
+        self.assertEqual(df.to_json(orient='records'), '[{"context_all_spear":true,"context_all_pots":null,"context_all_knife":null,"context_all_count":1,"context_utilitarian_knife":null,"context_utilitarian_count":0,"context_textile_count":0,"context_equestrian_count":0,"context_economic_count":0,"context_organic_material_count":0,"context_appearance_count":0,"context_burial_container_count":0,"context_weapons_spear":1,"context_weapons_count":1,"context_iron_fragment_count":0,"context_miscellaneous_count":0}]')
+
+    def test_known_context_to_group(self):
+        known_context_keys = {'knife': set(['utilitarian']),
+                              'Whetstone': set(['utilitarian']),
+                              'Awl': set(['utilitarian']),
+                              'SCISSORS': set(['utilitarian']),
+                              'iron_object': set(),  # TODO
+                              'textile': set(['textile']),
+                              'spindle_whorl': set(),
+                              'brooch': set(['appearance']),
+                              'bracelet': set(['appearance']),
+                              'lock': set(['miscellaneous']),
+                              'keys': set(['miscellaneous']),
+                              'Sword': set(['weapons']),
+                              'axe': set(['weapons']),
+                              'Iron_pole': set(),
+                              'spear': set(['weapons']),
+                              'shield_boss': set(),
+                              'cloak_fastener': set(),
+                              'iron_ring': set(),
+                              'iron_nail/rivets': set(),
+                              'transport': set(),
+                              'Horse_equipment': set(),
+                              'coffin_substitute': set(),
+                              'coffin': set(['burial_container']),
+                              'Mounts': set(),
+                              'thors_hammer': set(),
+                              'beads': set(['appearance']),
+                              'unidentified_bronze': set(),
+                              'Bronze_disk': set(),
+                              'SCALES': set(['economic']),
+                              'coins': set(['economic']),
+                              'VESSEL': set(['utilitarian']),
+                              'comb': set(['appearance']),
+                              'buckle': set(['appearance']),
+                              'pot_sherd': set(),
+                              'FLINT_FLAKES': set(),
+                              'charcoal': set(),
+                              'burned_bones': set(),
+                              'quartz': set(['miscellaneous']),
+                              'shell': set(['organic_material']),
+                              'needle': set(['textile']),
+                              'Flower': set(['organic_material']),
+                              'animal_remains': set(),
+                              'Body_Position': set(),
+                              'disturbed': set(),
+                              'decapitation': set(),
+                              'double_grave': set(),
+                              'Body_orientation': set(),
+                              'Stone_layer': set()}
+        for known_key, known_groups in known_context_keys.items():
+            actual_groups = Context.group(known_key)
+            self.assertEqual(actual_groups, known_groups, msg=f'{known_key} -> {actual_groups} != {known_groups}')
 
 
 def main():
